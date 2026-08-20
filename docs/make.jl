af55d14f1db9e3c7e69dd1ea10b3a2ba92c442bc
YAMAMOTO, Takanori <106473329+yamanori99@users.noreply.github.com>
2026-08-20 20:56:03 +0900

    Add Google Search Console verification meta tag (#149)

--- docs/make.jl
+++ docs/make.jl
@@ -11,7 +11,13 @@ makedocs(;
         prettyurls=get(ENV, "CI", nothing) == "true",
         canonical="https://yamanori99.github.io/DistSSHKit.jl",
         edit_link="main",
-        assets=["assets/custom.css"],
+        assets=[
+            "assets/custom.css",
+            # Search Console (URL-prefix: https://yamanori99.github.io/DistSSHKit.jl/).
+            RawHTMLHeadContent(
+                """<meta name="google-site-verification" content="frfWUqaHuYYDmZzSSnBhfguS0Y5YC6zssij5qAot6ww" />""",
+            ),
+        ],
     ),
     pages=[
         "Introduction" => "index.md",
